FujiNetWIFI/fujinet-config

Add booting of Lobby sprintf/stdio removal

src/atari/input.c
@@ -6,7 +6,6 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <atari.h>
 #include <conio.h>
 #include <string.h>
@@ -314,6 +313,22 @@ HDSubState input_hosts_and_devices_hosts(void)
   case 'C':
     state = SHOW_INFO;
     return HD_DONE;
+  case 'L':
+    // boot lobby.
+    memset(temp, 0, sizeof(temp));
+    screen_puts(0,24,"Boot Lobby Y/N? ");
+    _screen_input(16,24,temp,2);
+    screen_clear_line(24);
+    switch (temp[0])
+    {
+      case 'Y':
+      case 'y':
+        mount_and_boot_lobby();
+        return HD_DONE;
+      default: // Anything but Y/y take to mean "no"
+        return HD_HOSTS;
+      }
+    return HD_HOSTS;
   case KCODE_RETURN:
     selected_host_slot = bar_get() - HOSTS_START_Y;
     if ( !wifiEnabled && strcmp(hostSlots[selected_host_slot],"SD") != 0) // Don't go in a TNFS host if wifi is disabled.
@@ -409,6 +424,23 @@ HDSubState input_hosts_and_devices_devices(void)
   case 'C':
     state = SHOW_INFO;
     return HD_DONE;
+  case 'L':
+    // boot lobby.
+    // boot lobby.
+    memset(temp, 0, sizeof(temp));
+    screen_puts(0,24,"Boot Lobby Y/N? ");
+    _screen_input(16,24,temp,2);
+    screen_clear_line(24);
+    switch (temp[0])
+    {
+      case 'Y':
+      case 'y':
+        mount_and_boot_lobby();
+        return HD_DONE;
+      default: // Anything but Y/y take to mean "no"
+        return HD_DEVICES;
+      }
+    return HD_DEVICES;
   case '!':
     mount_and_boot();
   default:

src/atari/mount_and_boot.c
@@ -6,15 +6,36 @@
 #include "screen.h"
 #include "../die.h"
 #include "atari_die.h"
-#include <stdio.h>
 #include <conio.h>
 
+void mount_and_boot_lobby(void)
+{
+    screen_mount_and_boot();
+    set_active_screen(SCREEN_MOUNT_AND_BOOT);
+    screen_clear();
+    screen_puts(3, 0, "Booting Lobby");
+
+
+//    if ( !io_mount_all() )
+//    {
+//        screen_error("ERROR MOUNTING ALL");
+//        wait_a_moment();
+//        state = HOSTS_AND_DEVICES;
+//    }
+//    else
+    {
+        while (!kbhit())
+        {
+        }
+        cgetc();
+
+        io_set_boot_mode(2);
+        cold_start();
+    }
+}
 
 void mount_and_boot(void)
 {
-    unsigned char i;
-    char temp[40];
-
     screen_mount_and_boot();
     set_active_screen(SCREEN_MOUNT_AND_BOOT);
 
@@ -35,39 +56,12 @@ void mount_and_boot(void)
     screen_clear();
     screen_puts(3, 0, "MOUNT AND BOOT");
 
-    for (i = 0; i < NUM_DEVICE_SLOTS; i++)
-    {
-        if (deviceSlots[i].hostSlot != 0xFF)
-        {
-            sprintf(temp, "Device %d: uses Host %d, mounting Host", i + 1, deviceSlots[i].hostSlot + 1);
-            screen_puts(0, 2 + i, temp);
-        }
-        else
-        {
-            sprintf(temp, "Device %d: empty", i + 1);
-            screen_puts(0, 2 + i, temp);
-        }
-    }
-
-    for (i = 0; i < NUM_DEVICE_SLOTS; i++)
-    {
-        if (deviceSlots[i].hostSlot != 0xFF)
-        {
-            sprintf(temp, "Device %d: Mounting image from Host %d", i + 1, deviceSlots[i].hostSlot + 1);
-            screen_puts(0, 4 + NUM_HOST_SLOTS + i, temp);
-        }
-        else
-        {
-            sprintf(temp, "Device %d: empty", i + 1);
-            screen_puts(0, 4 + NUM_HOST_SLOTS + i, temp);
-        }
-    }
+    screen_puts(5, 0, "Mounting all Host and Device Slots");
 
     if ( !io_mount_all() )
     {
         screen_error("ERROR MOUNTING ALL");
         wait_a_moment();
-        //die();
         state = HOSTS_AND_DEVICES;
     }
     else

diggerbonk/fujinet-config-dtm

src/atari/input.c
@@ -6,7 +6,6 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <atari.h>
 #include <conio.h>
 #include <string.h>
@@ -315,6 +314,22 @@ HDSubState input_hosts_and_devices_hosts(void)
   case 'C':
     state = SHOW_INFO;
     return HD_DONE;
+  case 'L':
+    // boot lobby.
+    memset(temp, 0, sizeof(temp));
+    cputsxy(0,24,"Boot Lobby Y/N? ");
+    _screen_input(16,24,temp,2);
+    screen_clear_line(24);
+    switch (temp[0])
+    {
+      case 'Y':
+      case 'y':
+        mount_and_boot_lobby();
+        return HD_DONE;
+      default: // Anything but Y/y take to mean "no"
+        return HD_HOSTS;
+      }
+    return HD_HOSTS;
   case KCODE_RETURN:
     selected_host_slot = bar_get() - HOSTS_START_Y;
     if ( !wifiEnabled && strcmp(hostSlots[selected_host_slot],"SD") != 0) // Don't go in a TNFS host if wifi is disabled.
@@ -410,6 +425,23 @@ HDSubState input_hosts_and_devices_devices(void)
   case 'C':
     state = SHOW_INFO;
     return HD_DONE;
+  case 'L':
+    // boot lobby.
+    // boot lobby.
+    memset(temp, 0, sizeof(temp));
+    cputsxy(0,24,"Boot Lobby Y/N? ");
+    _screen_input(16,24,temp,2);
+    screen_clear_line(24);
+    switch (temp[0])
+    {
+      case 'Y':
+      case 'y':
+        mount_and_boot_lobby();
+        return HD_DONE;
+      default: // Anything but Y/y take to mean "no"
+        return HD_DEVICES;
+      }
+    return HD_DEVICES;
   case '!':
     mount_and_boot();
   default:

src/atari/mount_and_boot.c
@@ -6,15 +6,35 @@
 #include "screen.h"
 #include "../die.h"
 #include "atari_die.h"
-#include <stdio.h>
 #include <conio.h>
 
+void mount_and_boot_lobby(void)
+{
+    screen_mount_and_boot();
+    screen_clear();
+    cputsxy(3, 0, "Booting Lobby");
+
+
+//    if ( !io_mount_all() )
+//    {
+//        screen_error("ERROR MOUNTING ALL");
+//        wait_a_moment();
+//        state = HOSTS_AND_DEVICES;
+//    }
+//    else
+    {
+        while (!kbhit())
+        {
+        }
+        cgetc();
+
+        io_set_boot_mode(2);
+        cold_start();
+    }
+}
 
 void mount_and_boot(void)
 {
-    unsigned char i;
-    char temp[40];
-
     screen_mount_and_boot();
 
     io_get_device_slots(&deviceSlots[0]);
@@ -34,39 +54,12 @@ void mount_and_boot(void)
     screen_clear();
     cputsxy(3, 0, "MOUNT AND BOOT");
 
-    for (i = 0; i < NUM_DEVICE_SLOTS; i++)
-    {
-        if (deviceSlots[i].hostSlot != 0xFF)
-        {
-            sprintf(temp, "Device %d: uses Host %d, mounting Host", i + 1, deviceSlots[i].hostSlot + 1);
-            cputsxy(0, 2 + i, temp);
-        }
-        else
-        {
-            sprintf(temp, "Device %d: empty", i + 1);
-            cputsxy(0, 2 + i, temp);
-        }
-    }
-
-    for (i = 0; i < NUM_DEVICE_SLOTS; i++)
-    {
-        if (deviceSlots[i].hostSlot != 0xFF)
-        {
-            sprintf(temp, "Device %d: Mounting image from Host %d", i + 1, deviceSlots[i].hostSlot + 1);
-            cputsxy(0, 4 + NUM_HOST_SLOTS + i, temp);
-        }
-        else
-        {
-            sprintf(temp, "Device %d: empty", i + 1);
-            cputsxy(0, 4 + NUM_HOST_SLOTS + i, temp);
-        }
-    }
+    cputsxy(5, 0, "Mounting all Host and Device Slots");
 
     if ( !io_mount_all() )
     {
         screen_error("ERROR MOUNTING ALL");
         wait_a_moment();
-        //die();
         state = HOSTS_AND_DEVICES;
     }
     else